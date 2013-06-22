1ba14913400e6f9731fe4c57189d41944b1e576e
DasPhilipp <Philipp@Philipps-MacBook-Air.local>
2013-06-22 20:36:23 +0200

    nur n paar kleine enderungen

--- SRC/main.lua
+++ SRC/main.lua
@@ -1,7 +1,7 @@
 require "player"
 
 function love.load()
-	local devmode = true
+	devmode = true
 	console = "Console: "
 	love.graphics.setBackgroundColor(255,255,255)
 
@@ -22,21 +22,26 @@ function love.update(dt)
 end
 
 function love.keypressed(key, unicode)
+
 	if key == "escape" then
 		love.event.push("quit")
 	end
+
 	if key == "return" then
 		gamestate = "playing"
 	end
+
 	if devmode == true then
 		if unicode > 31 and unicode < 127 then
         	console = console .. string.char(unicode)
     	end
     end
+
 end
 
 
 function love.draw()
+
 	if gamestate == "menu" then 
 		love.graphics.draw(pic.logo, 60, 125)
 	end
@@ -47,7 +52,8 @@ function love.draw()
 		love.graphics.setColor(255,255,255)
 		love.graphics.rectangle("fill", 0, 580, 800, 20)
 	end
-if devmode == true then
+
+	if devmode == true then
 		love.graphics.setColor(255,255,255)
 		love.graphics.printf(console, 0, 0, 800)
 	end
